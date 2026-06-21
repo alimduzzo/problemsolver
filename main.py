860f244b30287ead31e286aa0d95eac1c0f4caaf
alimduzzo <youareu36@gmail.com>
2026-06-21 23:36:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import undetected_chromedriver as uc
+#import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
 import re
 import json
